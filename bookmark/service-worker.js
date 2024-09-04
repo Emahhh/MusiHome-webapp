2fedf29b51c71b29c75a93ac3cdc459e8124a0d5
Emanuele B. <buonaccorsiemanuele@gmail.com>
2024-09-04 21:18:51 +0200

    trying to fix cache with event

--- bookmark/service-worker.js
+++ bookmark/service-worker.js
@@ -21,21 +21,29 @@ self.addEventListener('fetch', function(event) {
 
             return fetch(fetchRequest).then(function(response) {
                 // Check if we received a valid response
-                if(!response || response.status !== 200 || response.type !== 'basic') {
+                if(!response || response.status !== 200) {
                     return response;
                 }
 
-                // Clone the response because it's a stream and can only be consumed once
-                var responseToCache = response.clone();
-
-                caches.open('music-bookmark').then(function(cache) {
-                    cache.put(event.request, responseToCache);
-                });
-
                 return response;
+            }).catch(function() {
+                // Network error, try to return the cached version
+                return caches.match(event.request);
             });
         })
     );
 });
+
+// Add this new event listener to cache the image from the URL parameter
+self.addEventListener('message', function(event) {
+    if (event.data && event.data.type === 'CACHE_IMAGE') {
+        event.waitUntil(
+            caches.open('music-bookmark').then(function(cache) {
+                return cache.add(event.data.url);
+            })
+        );
+    }
+});
+
 
 
