5934de6bd1cc93c48e05248b9f1490a63e479c75
Emanuele B. <buonaccorsiemanuele@gmail.com>
2024-10-20 23:52:14 +0200

    ridirect più veloce, sempre senza usare la pagina

--- bookmark/service-worker.js
+++ bookmark/service-worker.js
@@ -21,26 +21,27 @@ self.addEventListener('fetch', function(event) {
 
     event.respondWith(
         caches.match(event.request).then(function(response) {
-            // Cache hit - return response
-            if (response) {
-                return response;
-            }
 
-            // Fetch from network
-            return fetch(event.request).catch(function() {
-
-                // Strategia redirect senza pagina: 
-                // non ho la pagina in cache e non ho internet, ma provo a fare un redirect comunque
-                // allora cerco l'url a cui voglio redirectare, prendendone l'url parameter chiamato 'url'
+            // prendo l'url a cui voglio redirectare, prendendone l'url parameter chiamato 'url'
+            // questa risposta non ha bisogno di connessione internet
+            {
                 const urlParams = new URLSearchParams(event.request.url.split('?')[1]);
                 const redirectUrl = urlParams.get('url');
                 if (redirectUrl) {
                     return Response.redirect(redirectUrl); // redirecto all'URL trovato
-                } else {
-                    return new Response('No internet connection and no redirect URL found.');
                 }
+            }
 
+            // se non riesco con la prima strategia, rispondo con la pagina cachata
+            if (response) {
+                return response;
+            } 
+
+            // altrimenti, rispondo con la pagina vera, presa dal network
+            return fetch(event.request).catch(function() {
+                return new Response('No internet connection and no redirect URL found.');
             });
+
         })
     );
 });
